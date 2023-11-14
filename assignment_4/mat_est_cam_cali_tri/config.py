52b858bdb48d611a839c2ec648cf218a4f5c7e22
Hongbo Zheng <hongboz2@illinois.edu>
2023-11-13 22:21:12 -0600

    start assignment 4 part 2

---
+++ assignment_4/mat_est_cam_cali_tri/config.py
@@ -0,0 +1,8 @@
+import logger
+
+# directories
+data_dir = "data"
+res_dir = "results"
+
+# test
+log_level = logger.LogLevel.info
---
+++ assignment_4/mat_est_cam_cali_tri/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import config
+import numpy
+import os
+import PIL.Image
+
+
+def main():
+
+    return
+
+if __name__ == "__main__":
+    main()
