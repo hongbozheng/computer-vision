000ed93f9abdd26356de0958ab5d050d89be8ef3
Hongbo Zheng <hongboz2@illinois.edu>
2023-11-26 22:16:40 -0600

    add filepaths

--- assignment_4/mat_est_cam_cali_tri/config.py
+++ assignment_4/mat_est_cam_cali_tri/config.py
@@ -1,8 +1,20 @@
 import logger
 
+
 # directories
 data_dir = "data"
+lib_dir = data_dir + "/library"
+lab_dir = data_dir + "/lab"
+gaudi_dir = data_dir + "/gaudi"
+house_dir = data_dir + "/house"
+filepaths = {"lab": [f"{lab_dir}/lab1.jpg", f"{lab_dir}/lab2.jpg", f"{lab_dir}/lab_matches.txt",
+                     f"{lab_dir}/lab_3d.txt"],
+             "lib": [f"{lib_dir}/library1.jpg", f"{lib_dir}/library2.jpg", f"{lib_dir}/library_matches.txt",
+                     f"{lib_dir}/library1_camera.txt", f"{lib_dir}/library2_camera.txt"],
+             "gaudi": [f"{gaudi_dir}/gaudi1.jpg", f"{gaudi_dir}/gaudi2.jpg"],
+             "house": [f"{house_dir}/house1.jpg", f"{house_dir}/house2.jpg"]}
 res_dir = "results"
 
 # test
+imwrite = False
 log_level = logger.LogLevel.info
